384cd810596eccbb02f797b0adcf4f3fe0730494
Maximilian Flis <maximilian.flis@gmail.com>
2017-08-08 22:14:48 +0200

    using mock-spawn and sinon to test mocked command execution

--- test/VBoxManage.spec.ts
+++ test/VBoxManage.spec.ts
@@ -2,13 +2,28 @@ import VBoxManage from "../lib/index";
 import 'mocha';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import * as sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import * as mockSpawn from 'mock-spawn';
+import child_process = require('child_process');
 
 const expect = chai.expect;
 
 describe('VBoxManage', () => {
+    let mySpawn = mockSpawn();
+    let originalSpawn = child_process.spawn;
+    let spawnSpy: sinon.SinonSpy;
+
     before(() => {
         chai.should();
+        chai.use(sinonChai);
         chai.use(chaiAsPromised);
+        child_process.spawn = mySpawn;
+        spawnSpy = sinon.spy(child_process, 'spawn');
+    });
+
+    beforeEach(() => {
+        spawnSpy.reset();
     });
 
     it('should support constructor options or option methods and both should return the same command line', () => {
@@ -26,32 +41,37 @@ describe('VBoxManage', () => {
         }
     });
 
-    describe('GuestControl', () => {
-        it('should support constructor options or option methods and both should return the same command line', () => {
-            let line1 = new VBoxManage().nologo().guestControl('the_vm').verbose().username('vagrant').password('vagrant').build().line;
-            let line2 = new VBoxManage('--nologo').guestControl('the_vm', '--verbose', '--username', '"vagrant"', '--password', '"vagrant"').build().line;
-            expect(line1).to.equal(line2);
-        });
+    it('should execute the vboxmanage command with the given params', () => {
+        let vboxmanage = new VBoxManage().nologo().version().build();
+        return expect(vboxmanage.execute()).to.be.fulfilled.then(() => {
+            expect(spawnSpy).to.be.calledWith(VBoxManage.determineBinaryPath(), ['--nologo', '--version']);
+        })
+    });
 
-        it('should create a guestcontrol command', () => {
-            let guestControl = new VBoxManage().nologo().guestControl('the_vm').verbose().username('vagrant').password('vagrant').build();
-            let commandline = `${VBoxManage.determineBinaryPath()} --nologo guestcontrol "the_vm" --verbose --username "vagrant" --password "vagrant"`;
-            expect(guestControl.line).to.equal(commandline);
+    describe('GuestControl', () => {
+        it('should execute the vboxmanage guestcontrol command with the given params', () => {
+            let vboxmanage = new VBoxManage().nologo().guestControl('the_vm').verbose().username('vagrant').password('vagrant').build();
+            return expect(vboxmanage.execute()).to.be.fulfilled.then(() => {
+                expect(spawnSpy).to.be.calledWith(VBoxManage.determineBinaryPath(), ['--nologo', 'guestcontrol', '"the_vm"', '--verbose', '--username', '"vagrant"', '--password', '"vagrant"']);
+            })
         });
 
         describe('Run', () => {
-            it('should create a run command with program and args either by specifying each separately or as arg list', () => {
+            it('should execute the vboxmanage guestcontrol run command with the given params', () => {
                 let vm = 'crazy_vm';
                 let windowsProgram = 'C:\\Windows\\System32\\wbem\\WMIC.exe';
-                let guestControl = new VBoxManage().nologo().guestControl(vm).verbose().username('vagrant').password('vagrant');
-                let run1 = guestControl.run().program(windowsProgram).arg('diskdrive').arg('get').arg('Manufacturer').arg('/value').build();
-                let run2 = guestControl.run('--wait-stdout', '--wait-stderr').program(windowsProgram).args('diskdrive', 'get', 'Manufacturer', '/value').build();
-
-                let commandline1 = `${VBoxManage.determineBinaryPath()} --nologo guestcontrol "${vm}" --verbose --username "vagrant" --password "vagrant" run -- "${windowsProgram}" diskdrive get Manufacturer /value`;
-                let commandline2 = `${VBoxManage.determineBinaryPath()} --nologo guestcontrol "${vm}" --verbose --username "vagrant" --password "vagrant" run --wait-stdout --wait-stderr -- "${windowsProgram}" diskdrive get Manufacturer /value`;
-                expect(run1.line).to.equal(commandline1);
-                expect(run2.line).to.equal(commandline2);
+                let vboxmanage = new VBoxManage().nologo()
+                    .guestControl(vm).verbose().username('vagrant').password('vagrant')
+                    .run().program(windowsProgram).arg('diskdrive').arg('get').arg('Manufacturer').arg('/value').build();
+                return expect(vboxmanage.execute()).to.be.fulfilled.then(() => {
+                    expect(spawnSpy).to.be.calledWith(VBoxManage.determineBinaryPath(), ['--nologo', 'guestcontrol', `"${vm}"`, '--verbose', '--username', '"vagrant"', '--password', '"vagrant"', 'run', '--', `"${windowsProgram}"`, 'diskdrive', 'get', 'Manufacturer', '/value']);
+                })
             });
         })
-    })
+    });
+
+    after(() => {
+        child_process.spawn = originalSpawn;
+        spawnSpy.restore();
+    });
 });
